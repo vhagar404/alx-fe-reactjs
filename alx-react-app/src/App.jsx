a9f336842819dae7f4cd2101a16183c3ff493b86
Goodluck <godspowergoodluck123@gmail.com>
2026-01-11 21:01:05 +0100

    Organize components in components folder and update imports

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,19 @@
-import UserProfile from './components/UserProfile';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div>
-      <UserProfile
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography"
-      />
-    </div>
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
   );
 }
 
 export default App;
+
 
 
