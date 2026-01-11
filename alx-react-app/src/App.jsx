0e3fd5315367e07502e4bae989d8a5b4f91103ee
Goodluck <godspowergoodluck123@gmail.com>
2026-01-11 17:43:33 +0100

    Add UserProfile component using props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,16 +1,17 @@
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
-      <Header />
-      <MainContent />
-      <Footer />
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
     </div>
   );
 }
 
 export default App;
+
 
