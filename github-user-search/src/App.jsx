b043dc5713e651a36105b0b3745ddb4680cc9ffb
Goodluck <godspowergoodluck123@gmail.com>
2026-01-29 12:39:01 +0100

    Setup React app for GitHub user search project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>GitHub User Search</h1>
+      <p>Search for GitHub profiles using the GitHub API.</p>
+    </div>
+  );
+}
+
+export default App;
+
