6ddcdd47d1b97dd1728f6e878dd6416f2b76c5f9
Goodluck <godspowergoodluck123@gmail.com>
2026-02-06 15:02:33 +0100

    Fix responsive image sizing with sm breakpoint

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,14 +8,16 @@ function UserProfile() {
   rounded-lg shadow-lg
 ">
       <img
-        src="https://via.placeholder.com/150"
-        alt="User"
-        className="
-          rounded-full mx-auto
-          w-24 h-24
-          md:w-36 md:h-36
-        "
-      />
+  src="https://via.placeholder.com/150"
+  alt="User"
+  className="
+    rounded-full mx-auto
+    w-24 h-24
+    sm:w-24 sm:h-24
+    md:w-36 md:h-36
+  "
+/>
+
 
       <h1 className="
         text-lg md:text-xl
