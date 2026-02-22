7eb000c08249c1fa477c7763127d6343eaf1ad28
Goodluck <godspowergoodluck123@gmail.com>
2026-02-22 20:14:54 +0100

    update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import TodoList from "./components/TodoList";
-import "./App.css"; // optional, for styling
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Todo List</h1>
+    <div>
       <TodoList />
     </div>
   );
